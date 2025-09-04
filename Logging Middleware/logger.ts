8d7493b3d6c98ab4a362b02c0c0241f1c123f932
Abhinav Reddy <abhinavreddy2307@gmail.com>
2025-09-04 12:18:26 +0530

    Final commit including middleware and screenshots

---
+++ Logging Middleware/logger.ts
@@ -0,0 +1,51 @@
+import axios from "axios";
+const LOG_API = "http://20.244.56.144/evaluation-service/logs";
+const levels = ["debug", "info", "warn", "error", "fatal"];
+const packages = [
+  "component",
+  "hook",
+  "page",
+  "state",
+  "style",
+  "auth",
+  "config",
+  "middleware",
+  "utils",
+  "App Component"
+];
+
+export async function logEvent(
+  level: string,
+  pkg: string,
+  message: string
+): Promise<void> {
+  if (!levels.includes(level)) {
+    console.error(`Invalid log level: ${level}`);
+    return;
+  }
+  if (!packages.includes(pkg)) {
+    console.error(`Invalid package: ${pkg}`);
+    return;
+  }
+
+  try {
+    const response = await axios.post(LOG_API, {
+      stack: "frontend",
+      level,
+      package: pkg,
+      message,
+    });
+
+    console.log("Log sent:", response.data);
+  } catch (error: any) {
+    if (axios.isAxiosError(error)) {
+      console.error(
+        "Failed to send log:",
+        error.response?.status,
+        error.response?.data || error.message
+      );
+    } else {
+      console.error("Unexpected error:", error);
+    }
+  }
+}
